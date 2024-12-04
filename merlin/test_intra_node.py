70e35f8b70e4245d4ebb23c236f42ac83f4cf1f9
Mu-Chi Chen <muchi674@gmail.com>
2024-12-05 02:19:03 +0800

    add more tests

--- merlin/test_intra_node.py
+++ merlin/test_intra_node.py
@@ -2,14 +2,16 @@
 import os
 import time
 
+from torch import nn
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
 
-def run(x: torch.Tensor, w: torch.Tensor, group):
+def run(x: torch.Tensor, w1: torch.Tensor, w2: torch.Tensor, w3: torch.Tensor, group):
     """Simple collective communication."""
-    res = x @ w.T
+    # res = (nn.functional.silu(x @ w1.T) * (x @ w3.T)) @ w2
+    res = x
     dist.all_reduce(res, op=dist.ReduceOp.SUM, group=group)
 
 
@@ -21,18 +23,28 @@ def init_process(rank, size, fn):
 
     device = torch.device(f"cuda:{rank}")
     group = dist.new_group([0, 1])
-    x = torch.ones((128, 4096), dtype=torch.bfloat16, device=device)
-    w = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (rank + 1)
-    for _ in range(10000):
-        fn(x, w, group)
+    x = torch.ones((1, 32), dtype=torch.bfloat16, device=device)
+    w1 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (rank + 1)
+    w2 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (rank + 2)
+    w3 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (rank + 3)
+
+    # warmup
+    for _ in range(1000):
+        fn(x, w1, w2, w3, group)
+
+    tic = time.time()
+
+    for _ in range(100000):
+        fn(x, w1, w2, w3, group)
+
+    print(f"AVG run latency: {((time.time() - tic) * 1000) / 100000} ms")
+    dist.destroy_process_group()
 
 
 if __name__ == "__main__":
     world_size = 2
     processes = []
     mp.set_start_method("spawn")
-
-    tic = time.time()
 
     for rank in range(world_size):
         p = mp.Process(target=init_process, args=(rank, world_size, run))
@@ -41,5 +53,3 @@ if __name__ == "__main__":
 
     for p in processes:
         p.join()
-
-    print(f"AVG run latency: {((time.time() - tic) * 1000) / 10000} ms")
