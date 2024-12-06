85c6c80226eab9e8cf9cfabb7db7d9c79ee62f0f
Mu-Chi Chen <muchi674@gmail.com>
2024-12-06 12:29:30 +0800

    [DEV] simpler inter_node test

--- merlin/test_inter_node.py
+++ merlin/test_inter_node.py
@@ -1,5 +1,29 @@
-from torch.distributed.device_mesh import init_device_mesh
-mesh_2d = init_device_mesh("cuda", mesh_shape=(2, 2), mesh_dim_names=("ep", "tp"))
+import os
 
-mesh_2d.get_group(mesh_dim="ep")
-mesh_2d.get_group(mesh_dim="tp")
+import torch
+import torch.distributed as dist
+
+# Environment variables set by torch.distributed.launch
+LOCAL_RANK = int(os.environ['LOCAL_RANK'])
+WORLD_SIZE = int(os.environ['WORLD_SIZE'])
+WORLD_RANK = int(os.environ['RANK'])
+
+def run():
+    tensor = torch.zeros(1)
+    device = torch.device("cuda:{}".format(LOCAL_RANK))
+    tensor = tensor.to(device)
+
+    if WORLD_RANK == 0:
+        for rank_recv in range(1, WORLD_SIZE):
+            dist.send(tensor=tensor, dst=rank_recv)
+            print('worker_{} sent data to Rank {}\n'.format(0, rank_recv))
+    else:
+        dist.recv(tensor=tensor, src=0)
+        print('worker_{} has received data from rank {}\n'.format(WORLD_RANK, 0))
+
+def init_processes():
+    dist.init_process_group("nccl", rank=WORLD_RANK, world_size=WORLD_SIZE)
+    run()
+
+if __name__ == "__main__":
+    init_processes()
