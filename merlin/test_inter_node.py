6ba2b8620f22faaee4cc7f54d0c04ae464301cf9
Mu-Chi Chen <muchi674@gmail.com>
2024-12-06 17:28:21 +0800

    debug

--- merlin/test_inter_node.py
+++ merlin/test_inter_node.py
@@ -10,7 +10,8 @@ LOCAL_RANK = int(os.environ["LOCAL_RANK"])
 WORLD_SIZE = int(os.environ["WORLD_SIZE"])
 WORLD_RANK = int(os.environ["RANK"])
 
-def expert(x: torch.Tensor, w1: torch.Tensor, w2: torch.Tensor, w3: torch.Tensor, group):
+# def expert(x: torch.Tensor, w1: torch.Tensor, w2: torch.Tensor, w3: torch.Tensor, group):
+def expert(x: torch.Tensor, group):
     """Simple collective communication."""
     # res = (F.silu(x @ w1.T) * (x @ w3.T)) @ w2
     dist.all_reduce(x, op=dist.ReduceOp.SUM, group=group)
@@ -18,19 +19,19 @@ def expert(x: torch.Tensor, w1: torch.Tensor, w2: torch.Tensor, w3: torch.Tensor
 def run():
     device = torch.device(f"cuda:{LOCAL_RANK}")
     x = torch.ones((1, 4096), dtype=torch.bfloat16, device=device)
-    w1 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 1)
-    w2 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 2)
-    w3 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 3)
+    # w1 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 1)
+    # w2 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 2)
+    # w3 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 3)
     group = dist.new_group(list(range(WORLD_SIZE)))
 
     # warmup
     for _ in range(1000):
-        expert(x, w1, w2, w3, group)
+        expert(x, group)
 
     tic = time.time()
 
     for _ in range(100000):
-        expert(x, w1, w2, w3, group)
+        expert(x, group)
 
     print(f"AVG run latency: {((time.time() - tic) * 1000) / 100000} ms")
 
