41ae0cffadc986d6d6e9467753003c0f624b8877
Mu-Chi Chen <muchi674@gmail.com>
2024-12-09 13:30:26 +0800

    sync

--- merlin/test_inter_node.py
+++ merlin/test_inter_node.py
@@ -1,7 +1,9 @@
 import os
+import time
 
 import torch
 import torch.distributed as dist
+import torch.nn.functional as F
 
 # Environment variables set by torch.distributed.launch
 LOCAL_RANK = int(os.environ["LOCAL_RANK"])
@@ -9,17 +11,39 @@ WORLD_SIZE = int(os.environ["WORLD_SIZE"])
 WORLD_RANK = int(os.environ["RANK"])
 
 
-def run():
-    device = torch.device(f"cuda:{LOCAL_RANK}")
-    tensor = torch.ones((1, 4096), dtype=torch.bfloat16, device=device)
-    group = dist.new_group(list(range(WORLD_SIZE)))
-    dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
-    print(tensor)
+# def run():
+#     device = torch.device(f"cuda:{WORLD_RANK}")
+#     tensor = torch.ones((1, 4096), dtype=torch.bfloat16, device=device)
+#     group = dist.new_group(list(range(WORLD_SIZE)))
+#     dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
+#     print(tensor)
 
+# def run(x: torch.Tensor, w1: torch.Tensor, w2: torch.Tensor, w3: torch.Tensor, group):
+#     """Simple collective communication."""
+#     # res = (nn.functional.silu(x @ w1.T) * (x @ w3.T)) @ w2
+#     res = x
+#     dist.all_reduce(res, op=dist.ReduceOp.SUM, group=group)
 
 def init_processes():
     dist.init_process_group("nccl", rank=WORLD_RANK, world_size=WORLD_SIZE)
-    run()
+
+    device = torch.device(f"cuda:{WORLD_RANK}")
+    group = dist.new_group([0, 1])
+    x = torch.ones((1, 32), dtype=torch.bfloat16, device=device)
+    w1 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 1)
+    w2 = torch.ones((4096, 14336), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 2)
+    w3 = torch.ones((14336, 4096), dtype=torch.bfloat16, device=device) * (WORLD_RANK + 3)
+
+    # warmup
+    for _ in range(10):
+        dist.all_reduce(x, op=dist.ReduceOp.SUM, group=group)
+
+    tic = time.time()
+
+    for _ in range(100):
+        dist.all_reduce(x, op=dist.ReduceOp.SUM, group=group)
+
+    print(f"AVG run latency: {((time.time() - tic) * 1000) / 100} ms")
     dist.destroy_process_group()
 
 
