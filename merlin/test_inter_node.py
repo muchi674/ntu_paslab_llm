8042caf388d69020f9a45abebd2ba510917dfb13
Mu-Chi Chen <muchi674@gmail.com>
2024-12-09 15:15:23 +0800

    sync

--- merlin/test_inter_node.py
+++ merlin/test_inter_node.py
@@ -17,15 +17,12 @@ def run(x: torch.Tensor, w1: torch.Tensor, w2: torch.Tensor, w3: torch.Tensor, g
 
 
 def init_processes():
-    dist.init_process_group(
-        "nccl",
-        rank=WORLD_RANK,
-        world_size=WORLD_SIZE,
-    )
-    print(int(os.environ["GROUP_RANK"]))
-
     device = torch.device(f"cuda:{LOCAL_RANK}")
-    group = dist.new_group(list(range(WORLD_SIZE)))
+    dist.init_process_group(
+        "nccl", rank=WORLD_RANK, world_size=WORLD_SIZE, device_id=device
+    )
+
+    group = dist.new_group(list(range(WORLD_SIZE)), use_local_synchronization=True)
     x = torch.ones((1, 4096), dtype=torch.bfloat16, device=device)
     w1 = (
         torch.ones((14336, 4096), dtype=torch.bfloat16, device=device)
