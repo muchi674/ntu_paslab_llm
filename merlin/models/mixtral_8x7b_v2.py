7d87bb5e3e1f843defe87e732ab91165e76ba8c0
Mu-Chi Chen <muchi674@gmail.com>
2024-12-27 20:06:28 +0800

    [DEBUG] tensor parllel attn hanging

--- merlin/models/mixtral_8x7b_v2.py
+++ merlin/models/mixtral_8x7b_v2.py
@@ -424,6 +424,8 @@ class MoeLayer(nn.Module):
         weights = F.softmax(weights, dim=1, dtype=torch.float).to(inputs.dtype)
         results = torch.zeros_like(inputs)
 
+        print("here")
+
         selected_experts = selected_experts.to("cpu")
         eis, bis, nes = [], [], []
         for ei in range(self.num_experts):
@@ -433,12 +435,20 @@ class MoeLayer(nn.Module):
                 bis.append(batch_idx.to(device=inputs.device))
                 nes.append(nth_expert.to(device=inputs.device))
 
+        print("here0")
+
         for ei, batch_idx, nth_expert in zip(eis, bis, nes):
             ey = self.experts.forward(self.li, ei, inputs[batch_idx])
             if ey is None:
                 continue
             results[batch_idx] += weights[batch_idx, nth_expert, None] * ey
+
+        print("here1")
+
         dist.all_reduce(results, op=dist.ReduceOp.SUM, group=self.group)
+
+        print("here2")
+
         return results
 
 
