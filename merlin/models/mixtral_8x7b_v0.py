8c28fa3ec948a7d278ef2f8e1e783f84e3480263
hosahochun <shaochunho@gmail.com>
2025-01-14 11:47:44 +0800

    [EXPERIMENT]

--- merlin/models/mixtral_8x7b_v0.py
+++ merlin/models/mixtral_8x7b_v0.py
@@ -416,10 +416,10 @@ class MoeLayer(nn.Module):
 
     def forward(self, inputs: torch.Tensor, is_prefill: bool, need_profile: bool) -> torch.Tensor:
         # computation
-        #start = torch.cuda.Event(enable_timing=True)
-        #end = torch.cuda.Event(enable_timing=True)
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
         
-        #start.record()
+        start.record()
         gate_logits = self.gate(inputs)
         weights, selected_experts = torch.topk(gate_logits, self.num_experts_per_tok)
         weights = F.softmax(weights, dim=1, dtype=torch.float).to(inputs.dtype)
@@ -437,29 +437,29 @@ class MoeLayer(nn.Module):
         for ei, batch_idx, nth_expert in zip(eis, bis, nes):
             ey = self.experts.forward(self.li, ei, inputs[batch_idx])
             results[batch_idx] += weights[batch_idx, nth_expert, None] * ey
-        #end.record()
+        end.record()
 
-        #torch.cuda.synchronize()
-        #if need_profile:
-        #    if is_prefill:
-        #        self.prefill_comp_time.append(start.elapsed_time(end))
-        #    else: 
-        #        self.decode_comp_time.append(start.elapsed_time(end))
+        torch.cuda.synchronize()
+        if need_profile:
+            if is_prefill:
+                self.prefill_comp_time.append(start.elapsed_time(end))
+            else: 
+                self.decode_comp_time.append(start.elapsed_time(end))
 
         # communication
-        #start = torch.cuda.Event(enable_timing=True)
-        #end = torch.cuda.Event(enable_timing=True)
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
 
-        #start.record()
+        start.record()
         dist.all_reduce(results, op=dist.ReduceOp.SUM, group=self.group)
-        #end.record()
+        end.record()
 
-        #torch.cuda.synchronize()
-        #if need_profile:
-        #    if is_prefill:
-        #        self.prefill_comm_time.append(start.elapsed_time(end))
-        #    else: 
-        #        self.decode_comm_time.append(start.elapsed_time(end))
+        torch.cuda.synchronize()
+        if need_profile:
+            if is_prefill:
+                self.prefill_comm_time.append(start.elapsed_time(end))
+            else: 
+                self.decode_comm_time.append(start.elapsed_time(end))
 
         return results
 
