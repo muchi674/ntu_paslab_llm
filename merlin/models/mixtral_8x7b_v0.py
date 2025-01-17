292a055af577317fbdc63db940e81b248a2201e3
hosahochun <shaochunho@gmail.com>
2025-01-17 11:15:29 +0800

    [EXPERIMENT] add micro benchmark

--- merlin/models/mixtral_8x7b_v0.py
+++ merlin/models/mixtral_8x7b_v0.py
@@ -416,10 +416,6 @@ class MoeLayer(nn.Module):
 
     def forward(self, inputs: torch.Tensor, is_prefill: bool, need_profile: bool) -> torch.Tensor:
         # computation
-        start = torch.cuda.Event(enable_timing=True)
-        end = torch.cuda.Event(enable_timing=True)
-        
-        start.record()
         gate_logits = self.gate(inputs)
         weights, selected_experts = torch.topk(gate_logits, self.num_experts_per_tok)
         weights = F.softmax(weights, dim=1, dtype=torch.float).to(inputs.dtype)
@@ -434,32 +430,32 @@ class MoeLayer(nn.Module):
                 bis.append(batch_idx.to(device=inputs.device))
                 nes.append(nth_expert.to(device=inputs.device))
 
+        torch.cuda.synchronize(device=inputs.device)
+        tic = time.time()
         for ei, batch_idx, nth_expert in zip(eis, bis, nes):
             ey = self.experts.forward(self.li, ei, inputs[batch_idx])
             results[batch_idx] += weights[batch_idx, nth_expert, None] * ey
-        end.record()
+        torch.cuda.synchronize(device=inputs.device)
+        toc = time.time()
 
-        torch.cuda.synchronize()
         if need_profile:
             if is_prefill:
-                self.prefill_comp_time.append(start.elapsed_time(end))
+                self.prefill_comp_time.append((toc-tic)*1000)
             else: 
-                self.decode_comp_time.append(start.elapsed_time(end))
+                self.decode_comp_time.append((toc-tic)*1000)
 
         # communication
-        start = torch.cuda.Event(enable_timing=True)
-        end = torch.cuda.Event(enable_timing=True)
-
-        start.record()
+        torch.cuda.synchronize(device=inputs.device)
+        tic = time.time()
         dist.all_reduce(results, op=dist.ReduceOp.SUM, group=self.group)
-        end.record()
+        torch.cuda.synchronize(device=inputs.device)
+        toc = time.time()
 
-        torch.cuda.synchronize()
         if need_profile:
             if is_prefill:
-                self.prefill_comm_time.append(start.elapsed_time(end))
+                self.prefill_comm_time.append((toc-tic)*1000)
             else: 
-                self.decode_comm_time.append(start.elapsed_time(end))
+                self.decode_comm_time.append((toc-tic)*1000)
 
         return results
 
