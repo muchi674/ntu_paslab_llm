0e9ad8f04b2c795140a90d01c38efa95c53acc6c
Mu-Chi Chen <muchi674@gmail.com>
2025-01-21 22:40:02 +0800

    [TEST] 3 nodes EP

--- merlin/models/mixtral_8x7b_v1.py
+++ merlin/models/mixtral_8x7b_v1.py
@@ -403,16 +403,13 @@ class Experts:
 
 
 class MoeLayer(nn.Module):
-    def __init__(
-        self, args: ModelArgs, li: int, gate: nn.Module, experts: Experts, group
-    ):
+    def __init__(self, args: ModelArgs, li: int, gate: nn.Module, experts: Experts):
         super().__init__()
         self.num_experts: int = args.moe["num_experts"]
         self.num_experts_per_tok: int = args.moe["num_experts_per_tok"]
         self.li = li
         self.gate = gate
         self.experts = experts
-        self.group = group
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         gate_logits = self.gate(inputs)
@@ -435,7 +432,7 @@ class MoeLayer(nn.Module):
                 continue
             results[batch_idx] += weights[batch_idx, nth_expert, None] * ey
 
-        dist.all_reduce(results, op=dist.ReduceOp.SUM, group=self.group)
+        dist.all_reduce(results, op=dist.ReduceOp.SUM)
         return results
 
 
@@ -454,7 +451,7 @@ class RMSNorm(torch.nn.Module):
 
 
 class TransformerBlock(nn.Module):
-    def __init__(self, args: ModelArgs, li: int, experts: Experts, group):
+    def __init__(self, args: ModelArgs, li: int, experts: Experts):
         super().__init__()
         self.attention = Attention(args)
         self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
@@ -464,7 +461,6 @@ class TransformerBlock(nn.Module):
             li=li,
             gate=nn.Linear(args.dim, args.moe["num_experts"], bias=False),
             experts=experts,
-            group=group,
         )
 
     def forward(
@@ -478,7 +474,7 @@ class TransformerBlock(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, args: ModelArgs, experts: Experts, group):
+    def __init__(self, args: ModelArgs, experts: Experts):
         super().__init__()
         self.args = args
         self._precomputed_freqs_cis: torch.Tensor = None
@@ -487,9 +483,7 @@ class Transformer(nn.Module):
         self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
         self.layers = nn.ModuleDict(
             {
-                str(li): TransformerBlock(
-                    args=args, li=li, experts=experts, group=group
-                )
+                str(li): TransformerBlock(args=args, li=li, experts=experts)
                 for li in range(args.n_layers)
             }
         )
@@ -542,7 +536,7 @@ class Transformer(nn.Module):
         return outs.float()
 
     @staticmethod
-    def load(model_path: Path, node_id: int, gpu: torch.device, group) -> "Transformer":
+    def load(model_path: Path, node_id: int, gpu: torch.device) -> "Transformer":
         model_args = ModelArgs.from_hf_config(get_json(model_path / "config.json"))
         non_experts = torch.load(
             model_path / "non-experts.pt",
@@ -558,7 +552,7 @@ class Transformer(nn.Module):
         )
 
         with torch.device("meta"):
-            model = Transformer(args=model_args, experts=Experts(experts), group=group)
+            model = Transformer(args=model_args, experts=Experts(experts))
         model.load_state_dict(non_experts, assign=True, strict=True)
 
         return model
@@ -569,7 +563,6 @@ def generate(
     prompts: List[str],
     tokenizer: MistralTokenizer,
     model: Transformer,
-    group,
     *,
     max_tokens: int,
     max_batch_size: int = 64,
@@ -609,7 +602,7 @@ def generate(
     last_positions = torch.tensor(seqlens, device=prelogits.device).cumsum(dim=0) - 1
     last_token_prelogits = prelogits.index_select(0, last_positions)
 
-    dist.barrier(group=group)
+    dist.barrier()
     prefill_time = time.time() - tic
     tic = time.time()
 
@@ -637,7 +630,7 @@ def generate(
         generated_tokens = []
     responses = [tokenizer.decode(y) for y in generated_tokens]
 
-    dist.barrier(group=group)
+    dist.barrier()
     decode_time = time.time() - tic
 
     return (
@@ -696,16 +689,14 @@ def main(
     dist.init_process_group(
         "nccl", rank=WORLD_RANK, world_size=WORLD_SIZE, device_id=gpu
     )
-    group = dist.new_group(list(range(WORLD_SIZE)), use_local_synchronization=True)
     tokenizer = MistralTokenizer.v1()
-    model = Transformer.load(Path(model_path), node_id, gpu, group)
+    model = Transformer.load(Path(model_path), node_id, gpu)
 
     # warmup
     generate(
         ["hello, how are you?"],
         tokenizer,
         model,
-        group,
         max_tokens=16,
         max_batch_size=1,
         # temperature=0,
@@ -729,7 +720,6 @@ def main(
             prompt_batch,
             tokenizer,
             model,
-            group,
             max_tokens=max_tokens,
             max_batch_size=len(prompt_batch),
             # temperature=0,
@@ -773,7 +763,7 @@ def main(
         print(f"avg decode throughput: {mean(decode_tps):.2f} t/s")
 
     torch.cuda.cudart().cudaProfilerStop()
-    dist.barrier(group=group)
+    dist.barrier()
     dist.destroy_process_group()
 
 
