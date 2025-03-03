d7b2873f07028ad74d14bc8d1c660f04ed1c4b60
Kyle355469 <kyle355469@gmail.com>
2025-03-03 13:12:24 +0800

    idiot

--- merlin/models/mixtral_8x7b_v1.py
+++ merlin/models/mixtral_8x7b_v1.py
@@ -468,7 +468,7 @@ class MoeLayer(nn.Module):
                 continue
             end_idx = start_idx + num_tokens
             tokens_for_this_expert = sorted_tokens[start_idx:end_idx]
-            expert_out = self.experts.forward(,self.li, i + self.expert_start_idx, tokens_for_this_expert)
+            expert_out = self.experts.forward(self.li, i + self.expert_start_idx, tokens_for_this_expert)
             outputs.append(expert_out)
             start_idx = end_idx
 
