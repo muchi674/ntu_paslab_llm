a4416369692532b76c9d5dde0f2fb7d5e2aa52b9
Mu-Chi Chen <muchi674@gmail.com>
2025-03-03 11:28:27 +0800

    adjust inter_node_comm microbenchmarks

--- merlin/perf_model/microbenchmarks/inter_node_comm.py
+++ merlin/perf_model/microbenchmarks/inter_node_comm.py
@@ -30,39 +30,34 @@ def print_and_save_res(
     print("-" * 20)
     print("data_size_bytes, latency_ms, ")
     for ins, latency in zip(inputs, avg_latencies):
-        ins = str(torch.numel(ins) * 2)  # bfloat16 -> 2 bytes
+        key = "-".join(str(d) for d in ins.shape)
         latency = round(latency, 3)
-        data[ins] = latency
-        print(f"{ins}, {latency}, ")
+        data[key] = latency
+        print(f"{key}, {latency}, ")
 
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-def init_processes(start_bsz: int, end_bsz: int):
+def init_processes(start_bsz: int, end_bsz: int, seqlen: int, model_dim: int):
     device = torch.device(f"cuda:{LOCAL_RANK}")
     dist.init_process_group(
         "nccl", rank=WORLD_RANK, world_size=WORLD_SIZE, device_id=device
     )
     global_map = get_global_map(device)
     inputs = [torch.ones((1,), dtype=torch.bfloat16, device=device)]
-    # batch_size = 1
-    # seq_len = 128
-    # while 2 * batch_size * seq_len * 4096 / 1024**2 <= max_mb:
-    #     inputs.append(
-    #         torch.ones((batch_size, seq_len, 4096), dtype=torch.bfloat16, device=device)
-    #     )
-    #     batch_size *= 2
     while start_bsz <= end_bsz:
         inputs.append(
-            torch.ones((start_bsz * 128, 4096), dtype=torch.bfloat16, device=device)
+            torch.ones(
+                (start_bsz, seqlen, model_dim), dtype=torch.bfloat16, device=device
+            )
         )
         start_bsz *= 2
 
     N = 4000
     avg_latencies = []  # in ms
 
-    for i, ins in enumerate(inputs):
+    for ins in inputs:
         # warmup
         for _ in range(2000):
             dist.all_reduce(ins, op=dist.ReduceOp.MAX)
@@ -70,60 +65,56 @@ def init_processes(start_bsz: int, end_bsz: int):
         if LOCAL_RANK == 0:
             print(f"working on inputs with: {ins.shape}")
 
-        if i > 0:
-            torch.cuda.cudart().cudaProfilerStart()
-
         tic = time.time()
         for _ in range(N):
             dist.all_reduce(ins, op=dist.ReduceOp.MAX)
         avg_latencies.append((time.time() - tic) * 1000 / N)
 
-        if i > 0:
-            torch.cuda.cudart().cudaProfilerStop()
+    if WORLD_RANK == 0:
+        print_and_save_res(
+            "INTER COLL COMM LATENCY", inputs, avg_latencies, "inter_coll_comm.json"
+        )
 
-    # if WORLD_RANK == 0:
-    #     print_and_save_res(
-    #         "INTER COLL COMM LATENCY", inputs, avg_latencies, "inter_coll_comm.json"
-    #     )
+    N = 3000
+    warmups = 600
+    avg_latencies = []  # in ms
+    receiver = 0
+    sender = torch.min(
+        global_map[global_map[:, 0] == 1][:, 1]
+    ).item()  # first rank of the second node
 
-    # N = 3000
-    # warmups = 600
-    # avg_latencies = []  # in ms
-    # receiver = 0
-    # sender = torch.min(
-    #     global_map[global_map[:, 0] == 1][:, 1]
-    # ).item()  # first rank of the second node
+    for ins in inputs:
+        if WORLD_RANK == sender or WORLD_RANK == receiver:
+            # warmup
+            for _ in range(warmups):
+                if WORLD_RANK == sender:
+                    ops = [dist.P2POp(dist.isend, ins, receiver)]
+                else:
+                    ops = [dist.P2POp(dist.irecv, ins, sender)]
+                for req in dist.batch_isend_irecv(ops):
+                    req.wait()
 
-    # for ins in inputs:
-    #     if WORLD_RANK == sender or WORLD_RANK == receiver:
-    #         print(f"{WORLD_RANK} working on {torch.numel(ins) * 2}")
-    #         # warmup
-    #         for _ in range(warmups):
-    #             if WORLD_RANK == sender:
-    #                 ops = [dist.P2POp(dist.isend, ins, receiver)]
-    #             else:
-    #                 ops = [dist.P2POp(dist.irecv, ins, sender)]
-    #             for req in dist.batch_isend_irecv(ops):
-    #                 req.wait()
+            if LOCAL_RANK == 0:
+                print(f"working on inputs with: {ins.shape}")
 
-    #         tic = time.time()
+            tic = time.time()
 
-    #         for _ in range(N):
-    #             if WORLD_RANK == sender:
-    #                 ops = [dist.P2POp(dist.isend, ins, receiver)]
-    #             else:
-    #                 ops = [dist.P2POp(dist.irecv, ins, sender)]
-    #             for req in dist.batch_isend_irecv(ops):
-    #                 req.wait()
+            for _ in range(N):
+                if WORLD_RANK == sender:
+                    ops = [dist.P2POp(dist.isend, ins, receiver)]
+                else:
+                    ops = [dist.P2POp(dist.irecv, ins, sender)]
+                for req in dist.batch_isend_irecv(ops):
+                    req.wait()
 
-    #         duration = time.time() - tic
-    #         avg_latencies.append(duration * 1000 / N)
-    #     dist.barrier()
+            duration = time.time() - tic
+            avg_latencies.append(duration * 1000 / N)
+        dist.barrier()
 
-    # if WORLD_RANK == 0:
-    #     print_and_save_res(
-    #         "AVG INTER P2P COMM LATENCY", inputs, avg_latencies, "inter_p2p_comm.json"
-    #     )
+    if WORLD_RANK == 0:
+        print_and_save_res(
+            "AVG INTER P2P COMM LATENCY", inputs, avg_latencies, "inter_p2p_comm.json"
+        )
 
     dist.barrier()
     dist.destroy_process_group()
@@ -133,6 +124,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--start-bsz", type=int, default=128)
     parser.add_argument("--end-bsz", type=int, default=128)
+    parser.add_argument("--seq-len", type=int)
+    parser.add_argument("--model-dim", type=int)
     args = parser.parse_args()
-    init_processes(args.start_bsz, args.end_bsz)
-    # torchrun --nnodes=2 --node-rank=0 --nproc-per-node=2 --master-addr=10.10.10.1 --master-port=9091 inter_node_comm.py
+    init_processes(args.start_bsz, args.end_bsz, args.seq_len, args.model_dim)
+    # torchrun --nnodes=2 --node-rank=0 --nproc-per-node=2 --master-addr=10.10.10.1 --master-port=9091 inter_node_comm.py --start-bsz 1 --end-bsz 128 seq-len 128 --model-dim 4096
