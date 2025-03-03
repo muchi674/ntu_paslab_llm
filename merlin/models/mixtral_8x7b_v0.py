057749e7764a351bf83ae21de67fea29abdb617f
Kyle355469 <kyle355469@gmail.com>
2025-03-03 17:00:52 +0800

    remove redundant code

--- merlin/models/mixtral_8x7b_v0.py
+++ merlin/models/mixtral_8x7b_v0.py
@@ -414,7 +414,6 @@ class MoeLayer(nn.Module):
         gate_logits = self.gate(inputs)
         topk_weight, topk_idx = torch.topk(gate_logits, self.num_experts_per_tok)
         topk_weight = F.softmax(topk_weight, dim=1, dtype=torch.float).to(inputs.dtype)
-        inputs_flat = inputs.view(-1, inputs.shape[-1])
         y = self.moe_infer(inputs, topk_idx, topk_weight).view(*orig_shape)
         dist.all_reduce(y, op=dist.ReduceOp.SUM, group=self.group)
         return y
