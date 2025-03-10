dc8bf8f3b032176a499f28f7ea73db24477a9f46
Mu-Chi Chen <muchi674@gmail.com>
2025-03-10 19:55:59 +0800

    [UNSOLVED] pending NCCL ops warning

--- merlin/models/mixtral_8x7b_graph.py
+++ merlin/models/mixtral_8x7b_graph.py
@@ -367,8 +367,6 @@ class Transformer(nn.Module):
                 )
                 args.append((decode_x, decode_storage_x))
 
-            torch.cuda.synchronize(device=self.device)
-            dist.barrier()
             graphed_callables = torch.cuda.make_graphed_callables(
                 tuple(callables),
                 tuple(args),
