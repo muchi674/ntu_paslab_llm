3f3b99765694a37c8ab6448eec0ef7dea88ee57a
hoshaochun <shaochunho@gmail.com>
2025-03-14 15:18:10 +0800

    organize files

--- merlin/misc_tests/expert_comp.py
+++ merlin/misc_tests/expert_comp.py
@@ -23,10 +23,9 @@ def print_and_save_res(
 dtype = torch.bfloat16
 device = torch.device("cuda:0")
 model_d = 4096
+interm_d = 14336
 n_layers = 32
-n_heads = 32
-n_kv_heads = 8
-head_dim = model_d // n_heads
+n_experts = 8
 n_warmups, n_samples = 5, 500
 
 def ceildiv(a, b):
@@ -35,49 +34,44 @@ def ceildiv(a, b):
 
 
 def run_workload(tp_size: int, n_tokens: int):
-    # adjusted_d = ceildiv(interm_d, tp_size)
+    adjusted_d = ceildiv(interm_d, tp_size)
     
     x = torch.ones((n_tokens, model_d), dtype=dtype, device=device)
     
     # TODO: how many weights do we need to load?
     # initialize weights
     n_ws = n_layers * 3
-    wqs = [torch.rand(size=(model_d, n_heads * head_dim // tp_size), dtype=dtype, device=device) for _ in range(n_ws)]
-    wks = [torch.rand(size=(model_d, n_kv_heads * head_dim // tp_size), dtype=dtype, device=device) for _ in range(n_ws)]
+    ws = [torch.rand(size=(model_d, adjusted_d), dtype=dtype, device=device) for _ in range(n_ws)]
 
     for _ in range(n_warmups):
-        for w in wqs:
-            y = x @ w
-        for w in wks:
+        for w in ws:
             y = x @ w
     
 
     torch.cuda.synchronize(device=device)
     tic = time.time()
     for _ in range(n_samples):
-        for w in wqs:
-            y = x @ w
-        for w in wks:
+        for w in ws:
             y = x @ w
 
     torch.cuda.synchronize(device=device)
-    latency = (time.time() - tic) * 2 * 1000 / (n_samples * n_ws)
+    latency = (time.time() - tic) * 1000 / (n_samples * n_ws)
     
     return latency
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max-n_tokens", type=int, default=4096)
+    parser.add_argument("--max-n_tokens", type=int, default=1024)
     args = parser.parse_args()
     
     avg_latencies = []
     input_sizes = []
     n_tokens = 1
     while n_tokens <= args.max_n_tokens:
-        latency = run_workload(tp_size=1, n_tokens=n_tokens)
-        avg_latencies.append(latency)
+        latency = run_workload(tp_size=6, n_tokens=n_tokens)
+        avg_latencies.append(latency*3)
         input_sizes.append(n_tokens)
         n_tokens = n_tokens*2
     
-    print_and_save_res("AVG ATTN QKVO MATMUL LATENCY", input_sizes, avg_latencies, "attn_comp.json")
+    print_and_save_res("AVG SINGLE EXPERT MATMUL LATENCY", input_sizes, avg_latencies, "expert_comp.json")
