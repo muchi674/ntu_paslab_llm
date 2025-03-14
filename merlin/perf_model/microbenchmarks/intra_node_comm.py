b904a79f67a396f55080a6c86aae0087435653a4
Mu-Chi Chen <muchi674@gmail.com>
2025-03-14 10:45:31 +0800

    update intra-comm microbenchmark

--- merlin/perf_model/microbenchmarks/intra_node_comm.py
+++ merlin/perf_model/microbenchmarks/intra_node_comm.py
@@ -17,16 +17,23 @@ def print_and_save_res(
     print("-" * 20)
     print("data_size_bytes, latency_ms, ")
     for ins, latency in zip(inputs, avg_latencies):
-        ins = str(torch.numel(ins) * 2)  # bfloat16 -> 2 bytes
+        key = "-".join(str(d) for d in ins.shape)
         latency = round(latency, 3)
-        data[ins] = latency
-        print(f"{ins}, {latency}, ")
+        data[key] = latency
+        print(f"{key}, {latency}, ")
 
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-def init_process(rank, world_size, max_mb):
+def init_process(
+    rank: int,
+    world_size: int,
+    start_bsz: int,
+    end_bsz: int,
+    seqlen: int,
+    model_dim: int,
+):
     """Initialize the distributed environment."""
     os.environ["MASTER_ADDR"] = "127.0.0.1"
     os.environ["MASTER_PORT"] = "9091"
@@ -35,30 +42,34 @@ def init_process(rank, world_size, max_mb):
         backend="nccl", rank=rank, world_size=world_size, device_id=device
     )
     inputs = [torch.ones((1,), dtype=torch.bfloat16, device=device)]
-    batch_size = 1
-    while 2 * batch_size * 1024 / 1024**2 <= max_mb:
+    while start_bsz <= end_bsz:
         inputs.append(
-            torch.ones((batch_size, 1024), dtype=torch.bfloat16, device=device)
+            torch.ones(
+                (start_bsz, seqlen, model_dim), dtype=torch.bfloat16, device=device
+            )
         )
-        batch_size *= 2
+        start_bsz *= 2
 
-    # N = 20000
-    # avg_latencies = []  # in ms
+    N = 20000
+    avg_latencies = []  # in ms
 
-    # for ins in inputs:
-    #     # warmup
-    #     for _ in range(2000):
-    #         dist.all_reduce(ins, op=dist.ReduceOp.MAX)
+    for ins in inputs:
+        # warmup
+        for _ in range(2000):
+            dist.all_reduce(ins, op=dist.ReduceOp.MAX)
 
-    #     tic = time.time()
-    #     for _ in range(N):
-    #         dist.all_reduce(ins, op=dist.ReduceOp.MAX)
-    #     avg_latencies.append((time.time() - tic) * 1000 / N)
+        if rank == 0:
+            print(f"[collective] working on inputs with: {ins.shape}")
 
-    # if rank == 0:
-    #     print_and_save_res(
-    #         "INTRA-NODE COMM LATENCY", inputs, avg_latencies, "intra_coll_comm.json"
-    #     )
+        tic = time.time()
+        for _ in range(N):
+            dist.all_reduce(ins, op=dist.ReduceOp.MAX)
+        avg_latencies.append((time.time() - tic) * 1000 / N)
+
+    if rank == 0:
+        print_and_save_res(
+            "INTRA-NODE COMM LATENCY", inputs, avg_latencies, "intra_coll_comm.json"
+        )
 
     N = 4000
     avg_latencies = []  # in ms
@@ -75,6 +86,9 @@ def init_process(rank, world_size, max_mb):
                 ops = [dist.P2POp(dist.irecv, ins, 0)]
             for req in dist.batch_isend_irecv(ops):
                 req.wait()
+
+        if rank == 0:
+            print(f"[P2P] working on inputs with: {ins.shape}")
 
         tic = time.time()
 
@@ -99,7 +113,10 @@ def init_process(rank, world_size, max_mb):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max-mb", type=int, default=128)
+    parser.add_argument("--start-bsz", type=int, default=128)
+    parser.add_argument("--end-bsz", type=int, default=128)
+    parser.add_argument("--seq-len", type=int)
+    parser.add_argument("--model-dim", type=int)
     args = parser.parse_args()
 
     world_size = torch.cuda.device_count()
@@ -107,7 +124,17 @@ if __name__ == "__main__":
     mp.set_start_method("spawn")
 
     for rank in range(world_size):
-        p = mp.Process(target=init_process, args=(rank, world_size, args.max_mb))
+        p = mp.Process(
+            target=init_process,
+            args=(
+                rank,
+                world_size,
+                args.start_bsz,
+                args.end_bsz,
+                args.seq_len,
+                args.model_dim,
+            ),
+        )
         p.start()
         processes.append(p)
 
