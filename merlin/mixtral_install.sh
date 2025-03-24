2b11cf4c159f0780d3614a97df2a1c8482a0499a
Mu-Chi Chen <muchi674@gmail.com>
2025-03-24 23:31:08 +0800

    update launch_nodes to migrate from conda to uv

---
+++ merlin/mixtral_install.sh
@@ -0,0 +1,4 @@
+# WARNING: do not run this file. apologies for the confusion
+uv pip install torch==2.5.1 --index-url https://download.pytorch.org/whl/cu121
+uv pip install mistral-common
+uv pip install termcolor
