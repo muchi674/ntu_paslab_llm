e2be97b44869c344185e661af2e12314ed74fe63
Mu-Chi Chen <muchi674@gmail.com>
2025-03-27 16:27:45 +0800

    switch back to numpy enum

--- merlin/models/mixtral_8x7b_graph.py
+++ merlin/models/mixtral_8x7b_graph.py
@@ -233,7 +233,7 @@ class MoeLayer(nn.Module):
         cnts = self.pinned_cnts
         tokens_per_expert = cnts[
             self.expert_start_idx + 1 : self.expert_end_idx + 1
-        ]
+        ].numpy()
         cnts = torch.cumsum(cnts, dim=0)
         fidx = cnts[self.expert_start_idx]
         bidx = cnts[self.expert_end_idx]
