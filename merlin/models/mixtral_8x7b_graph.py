ec35ec99a8361d318f3b95c1aaa9d353890ba3b7
Mu-Chi Chen <muchi674@gmail.com>
2025-03-28 16:00:20 +0800

    old graph debug

--- merlin/models/mixtral_8x7b_graph.py
+++ merlin/models/mixtral_8x7b_graph.py
@@ -478,6 +478,7 @@ class Transformer(nn.Module):
                 self.layers["0"].first_decode_graphable,
             ),
         )
+        # without this causes cublas_status_not_initialized error
         res_r, topk_weight, cnts, idxs = func(h, next_h)
         graphs.append(torch.cuda.CUDAGraph())
         with torch.cuda.graph(graphs[-1], pool=pool):  # share memory pool
