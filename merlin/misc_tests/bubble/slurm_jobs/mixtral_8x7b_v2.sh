34a227a58f7bd3d1915551cbaef3841aa9228283
eric891224 <s96006730@gmail.com>
2025-03-31 15:33:43 +0800

    h100 miscs

--- merlin/misc_tests/bubble/slurm_jobs/mixtral_8x7b_v2.sh
+++ merlin/misc_tests/bubble/slurm_jobs/mixtral_8x7b_v2.sh
@@ -31,7 +31,7 @@
 # done
 
 # for ((bs = 1; bs <= 256; bs=bs*2))
-for bs in 128 256;
+for bs in 128 128;
 do
     echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     echo "BATCH_SIZE=$bs, NODE_ID=$SLURM_NODEID"
