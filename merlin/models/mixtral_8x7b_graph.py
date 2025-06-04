76f466190bfe65abeee4aa258a2f4083320f3896
Mu-Chi Chen <muchi674@gmail.com>
2025-06-04 16:17:50 +0800

    add inter-TP suport

--- merlin/models/mixtral_8x7b_graph.py
+++ merlin/models/mixtral_8x7b_graph.py
@@ -88,7 +88,8 @@ class ModelArgs:
     last_layer: int = None
     has_pp: bool = False
     parallel_experts: bool = False
-    parallel_attn: bool = False
+    inter_parallel_attn: bool = False
+    intra_parallel_attn: bool = False
 
     @classmethod
     def from_hf_config(cls, params: dict):
@@ -349,7 +350,6 @@ class TransformerBlock(nn.Module):
 
     def first_prefill_graphable_intra_attn(self, x: torch.Tensor, next_h: torch.Tensor):
         r = self.prefill_attn(x)
-        # WARNING: assumes attention is intra-node TP
         dist.all_reduce(r, op=dist.ReduceOp.SUM, group=self.local_group)
         return self.get_routings(x, r, next_h)
 
@@ -365,6 +365,21 @@ class TransformerBlock(nn.Module):
     ):
         return self.first_prefill_graphable_intra_attn(
             self.moe_intra_allreduce(h, r), next_h
+        )
+
+    # --------------------------------------------------
+    # PREFILL, INTER-TP-ATTN
+
+    def first_prefill_graphable_inter_attn(self, x: torch.Tensor, next_h: torch.Tensor):
+        r = self.prefill_attn(x)
+        dist.all_reduce(r, op=dist.ReduceOp.SUM)
+        return self.get_routings(x, r, next_h)
+
+    def subseq_prefill_graphable_inter_attn_inter_moe(
+        self, h: torch.Tensor, r: torch.Tensor, next_h: torch.Tensor
+    ):
+        return self.first_prefill_graphable_inter_attn(
+            self.moe_inter_allreduce(h, r), next_h
         )
 
     # ==================================================
@@ -393,7 +408,6 @@ class TransformerBlock(nn.Module):
 
     def first_decode_graphable_intra_attn(self, x: torch.Tensor, next_h: torch.Tensor):
         r = self.decode_attn(x)
-        # WARNING: assumes attention is intra-node TP
         dist.all_reduce(r, op=dist.ReduceOp.SUM, group=self.local_group)
         return self.get_routings(x, r, next_h)
 
@@ -409,6 +423,21 @@ class TransformerBlock(nn.Module):
     ):
         return self.first_decode_graphable_intra_attn(
             self.moe_intra_allreduce(h, r), next_h
+        )
+
+    # --------------------------------------------------
+    # DECODE, INTER-TP-ATTN
+
+    def first_decode_graphable_inter_attn(self, x: torch.Tensor, next_h: torch.Tensor):
+        r = self.decode_attn(x)
+        dist.all_reduce(r, op=dist.ReduceOp.SUM)
+        return self.get_routings(x, r, next_h)
+
+    def subseq_decode_graphable_inter_attn_inter_moe(
+        self, h: torch.Tensor, r: torch.Tensor, next_h: torch.Tensor
+    ):
+        return self.first_decode_graphable_inter_attn(
+            self.moe_inter_allreduce(h, r), next_h
         )
 
     # ****************************************************************************************************
@@ -535,16 +564,20 @@ class Transformer(nn.Module):
             idx = 0
             if not prefill:
                 idx += len(options) // 2
-            if self.args.parallel_attn:
+            if self.args.inter_parallel_attn:
                 idx += 1
+            elif self.args.intra_parallel_attn:
+                idx += 2
             return options[idx]
 
         def select_subseq_graphable(prefill: bool, options: tuple):
             idx = 0
             if not prefill:
                 idx += len(options) // 2
-            if self.args.parallel_attn:
+            if self.args.inter_parallel_attn:
                 idx += 3
+            elif self.args.intra_parallel_attn:
+                idx += 4
             elif self.args.parallel_experts:
                 idx += 1
             else:
@@ -607,8 +640,10 @@ class Transformer(nn.Module):
             prefill,
             (
                 self.layers[k].first_prefill_graphable,
+                self.layers[k].first_prefill_graphable_inter_attn,
                 self.layers[k].first_prefill_graphable_intra_attn,
                 self.layers[k].first_decode_graphable,
+                self.layers[k].first_decode_graphable_inter_attn,
                 self.layers[k].first_decode_graphable_intra_attn,
             ),
         )
@@ -631,11 +666,13 @@ class Transformer(nn.Module):
                     self.layers[k].subseq_prefill_graphable,
                     self.layers[k].subseq_prefill_graphable_inter_moe,
                     self.layers[k].subseq_prefill_graphable_intra_moe,
+                    self.layers[k].subseq_prefill_graphable_inter_attn_inter_moe,
                     self.layers[k].subseq_prefill_graphable_intra_attn_inter_moe,
                     self.layers[k].subseq_prefill_graphable_intra_attn_intra_moe,
                     self.layers[k].subseq_decode_graphable,
                     self.layers[k].subseq_decode_graphable_inter_moe,
                     self.layers[k].subseq_decode_graphable_intra_moe,
+                    self.layers[k].subseq_decode_graphable_inter_attn_inter_moe,
                     self.layers[k].subseq_decode_graphable_intra_attn_inter_moe,
                     self.layers[k].subseq_decode_graphable_intra_attn_intra_moe,
                 ),
@@ -778,21 +815,23 @@ class Mixtral8x7B:
         ):
             model_args.parallel_experts = True
 
-        # check if intra-node TP is applied on attention
-        if (
-            non_experts[f"layers.{model_args.first_layer}.attention.wq.weight"].shape[0]
-            < model_args.n_heads * model_args.head_dim
-        ):
-            assert model_args.n_heads % LOCAL_WORLD_SIZE == 0
-            assert model_args.n_kv_heads % LOCAL_WORLD_SIZE == 0
-            model_args.n_heads //= LOCAL_WORLD_SIZE
-            model_args.n_kv_heads //= LOCAL_WORLD_SIZE
-            model_args.parallel_attn = True
+        # check if TP is applied on attention
+        org_wq_out_dim = model_args.n_heads * model_args.head_dim
+        loc_wq_out_dim = non_experts[
+            f"layers.{model_args.first_layer}.attention.wq.weight"
+        ].shape[0]
+        attn_tp_size = org_wq_out_dim // loc_wq_out_dim
+        model_args.n_heads //= attn_tp_size
+        model_args.n_kv_heads //= attn_tp_size
+        if attn_tp_size == WORLD_SIZE:
+            model_args.inter_parallel_attn = True
+        elif attn_tp_size == LOCAL_WORLD_SIZE:
+            model_args.intra_parallel_attn = True
 
         comms: list
         if (
             model_args.has_pp and model_args.parallel_experts
-        ) or model_args.parallel_attn:
+        ) or model_args.intra_parallel_attn:
             global_map = torch.zeros((WORLD_SIZE, 2), dtype=torch.int64, device=device)
             local_map = torch.tensor(
                 [node_id, WORLD_RANK], dtype=torch.int64, device=device
