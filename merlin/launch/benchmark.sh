1fe5bc33d8db18a439f74f75aad5f836663c751c
Mu-Chi Chen <muchi674@gmail.com>
2025-06-05 15:48:32 +0800

    minor adjustments

--- merlin/launch/benchmark.sh
+++ merlin/launch/benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-n_prompts=32
-batch_size=1
+n_prompts=1024
+batch_size=32
 
-while [[ $n_prompts -le 512 && $batch_size -le 16 ]]; do
+while [[ $n_prompts -le 1024 && $batch_size -le 32 ]]; do
 
     jq ".shared_exec_args.n_prompts = $n_prompts" launch_config.json > tmp.$$.json && mv tmp.$$.json launch_config.json
     jq ".shared_exec_args.batch_size = $batch_size" launch_config.json > tmp.$$.json && mv tmp.$$.json launch_config.json
--- merlin/models/mixtral_8x7b_graph.py
+++ merlin/models/mixtral_8x7b_graph.py
@@ -1110,7 +1110,7 @@ def main(
         if WORLD_RANK == 0:
             prefill_tp = n_p_tkns / prefill_time
             decode_tp = n_gen_tkns / decode_time
-            if n_gen_tkns / bsz > max_gen_len * 0.9:
+            if n_gen_tkns / bsz > max_gen_len * 0.75:
                 prefill_tps.append(prefill_tp)
                 decode_tps.append(decode_tp)
 
