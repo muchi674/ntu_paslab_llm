b0a7424ead3403c36d50cca82566c10331c5aab6
Kyle355469 <kyle355469@gmail.com>
2025-06-10 20:58:51 +0800

    inter tp intra attn

--- merlin/slurm/mixtral_part_run.sh
+++ merlin/slurm/mixtral_part_run.sh
@@ -5,8 +5,8 @@ which python
 SIF=/home/u3114747/paslab_llm.sif
 SINGULARITY="singularity run --nv $SIF"
 
-PRL_MODEL_PATH="/home/u3114747/m822_parallel/inter-ep"
-DESIGN_PATH="/home/u3114747/ntu_paslab_llm/merlin/partitioners/designs/8x22b-inter-ep.json"
+PRL_MODEL_PATH="/home/u3114747/m822_parallel/inter-tp-intra-attn"
+DESIGN_PATH="/home/u3114747/ntu_paslab_llm/merlin/partitioners/designs/8x22b-inter-tp-intra-attn.json"
 PROMPT_PATH="/home/u3114747/ntu_paslab_llm/merlin/prompts/mixtral_8x7b_128.json"
 
 N_PROMPTS=32
