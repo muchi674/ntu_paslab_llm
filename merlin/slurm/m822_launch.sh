7daae412bd538f0df945a8cbd9f466dcb696d9f3
Kyle355469 <kyle355469@gmail.com>
2025-06-10 14:00:58 +0800

    path change

--- merlin/slurm/m822_launch.sh
+++ merlin/slurm/m822_launch.sh
@@ -3,11 +3,11 @@
 #Batch Job Paremeters
 #SBATCH --account=GOV113121
 #SBATCH --partition=normal
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1             # one torchrun per node https://stackoverflow.com/a/65897194
-#SBATCH --cpus-per-task=72
+#SBATCH --cpus-per-task=36
 #SBATCH --mem=200G
-#SBATCH --gpus-per-node=6
+#SBATCH --gpus-per-node=4
 #SBATCH --time=02:00:00                 # total run time limit (HH:MM:SS)
 
 # net
@@ -28,10 +28,10 @@ export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
 export OMP_NUM_THREADS=8
 export RDZV_ID=$RANDOM
 
-cd /home/paslab504llm/ntu_paslab_llm/merlin
+cd /home/u3114747/ntu_paslab_llm/merlin
 source .venv/bin/activate
 
-CMD="bash /home/paslab504llm/ntu_paslab_llm/merlin/slurm/mixtral_run.sh"
+CMD="bash /home/u3114747/ntu_paslab_llm/merlin/slurm/mixtral_run.sh"
 
 # https://discuss.pytorch.org/t/distributed-training-on-slurm-cluster/150417/8
 echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
--- merlin/slurm/mixtral_run.sh
+++ merlin/slurm/mixtral_run.sh
@@ -2,10 +2,13 @@
 
 which python
 
-OG_MODEL_PATH="/home/paslab504llm/Mixtral-8x22B-Instruct-v0.1"
-PRL_MODEL_PATH="/home/paslab504llm/m822_parallel/intra-pp"
-DESIGN_PATH="/home/paslab504llm/ntu_paslab_llm/merlin/partitioners/designs/pp.json"
-PROMPT_PATH="/home/paslab504llm/ntu_paslab_llm/merlin/prompts/mixtral_8x7b_128.json"
+SIF=/home/u3114747/paslab_llm.sif
+SINGULARITY="singularity run --nv $SIF"
+
+OG_MODEL_PATH="/home/u3114747/Mixtral-8x22B-Instruct-v0.1"
+PRL_MODEL_PATH="/home/u3114747/m822_parallel/intra-pp"
+DESIGN_PATH="/home/u3114747/ntu_paslab_llm/merlin/partitioners/designs/pp.json"
+PROMPT_PATH="/home/u3114747/ntu_paslab_llm/merlin/prompts/mixtral_8x7b_128.json"
 
 mkdir $PRL_MODEL_PATH
 cp $OG_MODEL_PATH/config.json $PRL_MODEL_PATH
@@ -18,7 +21,7 @@ cp $OG_MODEL_PATH/*token* $PRL_MODEL_PATH
 #     --output-path $PRL_MODEL_PATH
 
 echo "[`date`] started partitioning weights"
-python /home/paslab504llm/ntu_paslab_llm/merlin/partitioners/mixtral_8x22b.py \
+$SINGULARITY python /home/u3114747/ntu_paslab_llm/merlin/partitioners/mixtral_8x22b.py \
     --model-path $OG_MODEL_PATH \
     --design-path $DESIGN_PATH \
     --output-path $PRL_MODEL_PATH \
@@ -30,13 +33,13 @@ BATCH_SIZE=1
 while [[ $N_PROMPTS -le 1024 && $BATCH_SIZE -le 32 ]]; do
     echo "N_PROMPTS: $N_PROMPTS, BATCH_SIZE: $BATCH_SIZE"
 
-    torchrun \
+    $SINGULARITY torchrun \
         --nnodes=$SLURM_JOB_NUM_NODES \
         --nproc-per-node=$SLURM_GPUS_PER_NODE \
         --rdzv_id $RDZV_ID \
         --rdzv_backend c10d \
         --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
-        /home/paslab504llm/ntu_paslab_llm/merlin/models/mixtral_8x7b_graph.py \
+        /home/u3114747/ntu_paslab_llm/merlin/models/mixtral_8x7b_graph.py \
         --model-path $PRL_MODEL_PATH \
         --prompt-path $PROMPT_PATH \
         --n-prompts $N_PROMPTS \
