82ce3d45ea4c7592aa2332dd2283e93f96a7d94d
Mu-Chi Chen <muchi674@gmail.com>
2025-06-11 15:28:35 +0800

    pp 3 nodes test

--- merlin/launch/benchmark.sh
+++ merlin/launch/benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-n_prompts=1024
-batch_size=32
+n_prompts=32
+batch_size=1
 
-while [[ $n_prompts -le 1024 && $batch_size -le 32 ]]; do
+while [[ "${n_prompts}" -le 256 && "${batch_size}" -le 8 ]]; do
 
     jq ".shared_exec_args.n_prompts = $n_prompts" launch_config.json > tmp.$$.json && mv tmp.$$.json launch_config.json
     jq ".shared_exec_args.batch_size = $batch_size" launch_config.json > tmp.$$.json && mv tmp.$$.json launch_config.json
@@ -12,7 +12,7 @@ while [[ $n_prompts -le 1024 && $batch_size -le 32 ]]; do
     
     python launch_nodes.py --launch-config ./launch_config.json
 
-    sleep 10m
+    sleep 20m
     n_prompts=$((n_prompts * 2))
     batch_size=$((batch_size * 2))
 done
