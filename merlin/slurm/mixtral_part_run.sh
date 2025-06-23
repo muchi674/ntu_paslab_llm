bf100016822f33fa561e1daa10872348de37cfcc
kyle35546 <kyle355469@gmail.com>
2025-06-23 14:12:52 +0800

    sync

--- merlin/slurm/mixtral_part_run.sh
+++ merlin/slurm/mixtral_part_run.sh
@@ -5,18 +5,38 @@ which python
 SIF=/home/u3114747/paslab_llm.sif
 SINGULARITY="singularity run --nv $SIF"
 
-PRL_MODEL_PATH="/home/u3114747/m822_parallel/pp"
-DESIGN_PATH="/home/u3114747/ntu_paslab_llm/merlin/partitioners/designs/8x22b-pp.json"
+PRL_MODEL_PATH="/home/u3114747/m822_parallel/inter-tp-intra-attn"
+DESIGN_PATH="/home/u3114747/ntu_paslab_llm/merlin/partitioners/designs/8x22b-inter-tp-intra-attn.json"
 PROMPT_PATH="/home/u3114747/ntu_paslab_llm/merlin/prompts/mixtral_8x7b_128.json"
 
 echo $PRL_MODEL_PATH
 
-N_PROMPTS=32
-BATCH_SIZE=1
+N_PROMPTS=512
+BATCH_SIZE=16
  echo "started running mixtral_8x7b_graph.py"
-while [[ $N_PROMPTS -le 1024 && $BATCH_SIZE -le 32 ]]; do
-    echo "N_PROMPTS: $N_PROMPTS, BATCH_SIZE: $BATCH_SIZE"
+PROFILE_NAME="nsys_output_n512_b16.qdrep"
 
+if [[ $LOCAL_RANK == 0 ]]; then
+    $SINGULARITY nsys profile \
+        --output "/home/u3114747/8x22_profile_results/${PROFILE_NAME}" \
+        --capture-range=cudaProfilerApi \
+        --capture-range-end=stop \
+        --sample=none \
+        --trace=cuda,nvtx,osrt \
+        torchrun \
+        --nnodes=$SLURM_JOB_NUM_NODES \
+        --nproc-per-node=$SLURM_GPUS_PER_NODE \
+        --rdzv_id $RDZV_ID \
+        --rdzv_backend c10d \
+        --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
+        /home/u3114747/ntu_paslab_llm/merlin/models/mixtral_8x7b_graph.py \
+        --model-path $PRL_MODEL_PATH \
+        --prompt-path $PROMPT_PATH \
+        --n-prompts $N_PROMPTS \
+        --batch-size $BATCH_SIZE \
+        --max-tokens 32 \
+        --hide-resp
+else
     $SINGULARITY torchrun \
         --nnodes=$SLURM_JOB_NUM_NODES \
         --nproc-per-node=$SLURM_GPUS_PER_NODE \
@@ -28,9 +48,6 @@ while [[ $N_PROMPTS -le 1024 && $BATCH_SIZE -le 32 ]]; do
         --prompt-path $PROMPT_PATH \
         --n-prompts $N_PROMPTS \
         --batch-size $BATCH_SIZE \
-        --max-tokens 128 \
+        --max-tokens 32 \
         --hide-resp
-
-    N_PROMPTS=$((N_PROMPTS * 2))
-    BATCH_SIZE=$((BATCH_SIZE * 2))
-done
+fi
