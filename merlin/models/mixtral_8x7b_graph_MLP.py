8dd14036156065efbe442c7d57cc36ba65e3b5b0
awdadawdad <yanwengai@gmail.com>
2025-10-07 19:22:09 +0800

    debug triton MLP

--- merlin/models/mixtral_8x7b_graph_MLP.py
+++ merlin/models/mixtral_8x7b_graph_MLP.py
@@ -1066,7 +1066,7 @@ class Transformer(nn.Module):
         adj_idxs.copy_(adj)
         graphs.append(torch.cuda.CUDAGraph())
         with torch.cuda.graph(graphs[-1], pool=pool):  # share memory pool
-            self.layers[k].feed_forward.experts_infer(res_r, topk_weight, offsets, adj_idxs, static_data_placeholder := torch.empty_like(res_r))
+            next_h.add_(0)
         static_data.append((h, res_r, topk_weight, offsets, adj_idxs))
 
         for li in range(self.args.first_layer + 1, self.args.last_layer + 1):
@@ -1094,7 +1094,7 @@ class Transformer(nn.Module):
             )
             graphs.append(torch.cuda.CUDAGraph())
             with torch.cuda.graph(graphs[-1], pool=graphs[-2].pool()):
-                self.layers[k].feed_forward.experts_infer(res_r, topk_weight, offsets, adj_idxs, static_data_placeholder := torch.empty_like(res_r))
+                next_h.add_(0)
             static_data.append((h, r, res_r, topk_weight, offsets, adj_idxs))
 
         h = next_h
