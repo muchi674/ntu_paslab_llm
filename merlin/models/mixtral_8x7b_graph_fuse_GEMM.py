247cace3674ab4e420dc7eaea5f7be58ef6d1cb1
awdadawdad <yanwengai@gmail.com>
2025-10-11 15:17:43 +0800

    debug fuse gate_up

--- merlin/models/mixtral_8x7b_graph_fuse_GEMM.py
+++ merlin/models/mixtral_8x7b_graph_fuse_GEMM.py
@@ -869,7 +869,9 @@ class MoeLayer(nn.Module):
         topk_weight, topk_ids = torch.topk(gate_logits, self.num_experts_per_tok)
         topk_weight = F.softmax(topk_weight, dim=1, dtype=torch.float).to(x.dtype)
         topk_weight = topk_weight.flatten().unsqueeze(dim=-1)
+        
         offsets = fused_count_and_offsets(topk_ids, self.num_experts)
+
         idxs = topk_ids.flatten().argsort()
         adj_idxs = torch.div(idxs, self.num_experts_per_tok, rounding_mode="floor")
         return x[adj_idxs], topk_weight[idxs], offsets, adj_idxs
